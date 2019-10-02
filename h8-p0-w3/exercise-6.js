c978686228f815a17c1c09da0b4e58b1230e658b
Hesa Muhammad <hesamilan@gmail.com>
2019-10-02 13:52:48 +0700

    done

---
+++ h8-p0-w3/exercise-6.js
@@ -0,0 +1,22 @@
+function angkaPalindrome(num) {
+    // you can only write your code here!
+    num++
+
+    var string = String(num);
+    var reversedString = '';
+
+    for (i = string.length - 1; i >= 0; i--){
+        reversedString += string[i];
+    }
+    if (string === reversedString)
+        return Number(string);
+    else 
+        return angkaPalindrome(num);
+  }
+  
+  // TEST CASES
+  console.log(angkaPalindrome(8)); // 9
+  console.log(angkaPalindrome(10)); // 11
+  console.log(angkaPalindrome(117)); // 121
+  console.log(angkaPalindrome(175)); // 181
+  console.log(angkaPalindrome(1000)); // 1001
