3a63c67af9488bf0784c8626be413bc5bc4c3a9d
Hesa Muhammad <hesamilan@gmail.com>
2019-10-08 22:54:03 +0700

    done

--- h8-p0-w3/exercise-15.js
+++ h8-p0-w3/exercise-15.js
@@ -1,6 +1,31 @@
 function groupAnimals(animals) {
-    // you can only write your code here!
+  // you can only write your code here!
+  var tmp;
+  for (var i = 0; i < animals.length; i++){
+    for (var j = 0; j < animals.length; j++){
+      if (animals[i][0] < animals [j][0]){
+        tmp = animals[j];
+        animals[j] = animals[i];
+        animals[i] = tmp;
+      }
+    }
   }
+  var result = [[]];
+  var index = 0;
+
+  result[0][0] = animals [0];
+
+  for (var k = 1; k < animals.length; k++){
+    if (animals[k][0] === result[index][0][0]){
+      result[index][result[index].length] = animals[k];
+    }
+    else {
+      index++;
+      result[index] = [animals[k]];
+    }
+  }
+  return result;
+}
   
   // TEST CASES
   console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
