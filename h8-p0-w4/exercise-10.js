0ef85ce14d73e27e2624b427b39da8bc6ea6052f
Hesa Muhammad <hesamilan@gmail.com>
2019-10-11 14:16:37 +0700

    hampir

--- h8-p0-w4/exercise-10.js
+++ h8-p0-w4/exercise-10.js
@@ -1,9 +1,32 @@
 function changeMe(arr) {
     // you can only write your code here!
-  }
+    var number = 1;
+
+    if (arr.length === 0) {
+      console.log('');
+    } else {
+      for (var i = 0; i < arr.length; i++){
+        var obj = {};
+        obj.firstName = [i][0];
+        obj.lastName = [i][1];
+        obj.gender = [i][2];
+
+        if (arr[i][3] !== undefined){
+          obj.age = new Date().getFullYear() - arr[i][3];
+        } else {
+          obj.age = 'Invalid Birth Year'
+        }
+
+        console.log(obj);
+        number++
+      }
+    }
+}
+  
   
   // TEST CASES
-  changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]); // 1. Christ Evans:
+  changeMe([["Christ", "Evans", "Male", 1982], ["Robert", "Downey", "Male"]]);
+  // 1. Christ Evans:
   // { firstName: 'Christ',
   //   lastName: 'Evans',
   //   gender: 'Male',
